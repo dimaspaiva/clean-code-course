949ff4c00226718e950e90313e2b0e6f776dc913
dimas <dimasalpaiva@gmail.com>
2025-10-12 22:07:53 -0300

    feat: add create and get account

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,25 @@
-import { sum } from "../src/main"
+import axios from "axios"
 
-describe('Main', () => {
-  it('should test', () => {
-    expect(sum(1, 1)).toBe(2)
+describe('Signup', () => {
+  it('should create an account', async () => {
+    const input = {
+      name: "John Doe",
+      email: "john.doe@email.com",
+      document: "97456321558",
+      password: "safePass123@"
+    }
+
+    const responseSignup = await axios.post("http://localhost:3000/signup", input)
+    const outputSignup = responseSignup.data
+
+    expect(outputSignup.accountId).toBeDefined();
+
+    const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`)
+    const outputGetAccount = responseGetAccount.data
+
+    expect(outputGetAccount.name).toBe(input.name)
+    expect(outputGetAccount.email).toBe(input.email)
+    expect(outputGetAccount.password).toBe(input.password)
+    expect(outputGetAccount.document).toBe(input.document)
   })
 })
